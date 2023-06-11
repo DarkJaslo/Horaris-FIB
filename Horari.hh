21d98f77d759f33a624baeb2a4bb29a449ce11a4
Jon <jon.campillo@gmail.com>
2023-06-11 13:35:53 +0200

    got rid of using namespace std

--- Horari.hh
+++ Horari.hh
@@ -4,7 +4,6 @@
 #include <vector>
 #include <string>
 #include <iostream>
-using namespace std;  //Yes, it's not like it will cause any errors here
 
 enum DiaSetmana{
   dilluns,
@@ -43,54 +42,49 @@ inline std::ostream& operator<<(std::ostream& os, const TipusClasse& t){
 class HoraClasse{
 public:
   HoraClasse(){}
-  HoraClasse(int hora, bool ocupada, string assignatura, int _grup, TipusClasse tipus, DiaSetmana dia) : _hora(hora), _ocupada(ocupada), ass(assignatura), _grup(_grup), _tipus(tipus), _dia(dia){}
-  string assignatura()const{ return ass; }
+  HoraClasse(int hora, int grup, TipusClasse tipus, DiaSetmana dia, bool ocupada, std::string assignatura) 
+    : _hora(hora), _grup(grup), _tipus(tipus), _dia(dia), _ocupada(ocupada), ass(assignatura){}
+  std::string assignatura()const{ return ass; }
   int grup()const{ return _grup; }
   DiaSetmana dia()const{ return _dia; }
-  int hora()const{return _hora; }
-  bool ocupada()const{return _ocupada;}
-  void ocupa(){
-    _ocupada = true;
-  }
-  TipusClasse tipus() const{
-    return _tipus;
-  }
+  int hora()const{ return _hora; }
+  bool ocupada()const{ return _ocupada; }
+  void ocupa(){ _ocupada = true; }
+  TipusClasse tipus() const{ return _tipus; }
   friend std::ostream& operator<<(std::ostream& os, const HoraClasse& h);
   void print() const{
-    cout << assignatura() << " " << grup() << tipus();
+    std::cout << assignatura() << " " << grup() << tipus();
   }
 private:
   int _hora;
-  bool _ocupada;
-  string ass;
   int _grup;
   TipusClasse _tipus;
   DiaSetmana _dia;
+  bool _ocupada;
+  std::string ass;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const HoraClasse& h){
-  os << h.ass << ": " << endl <<
-  h._grup << h._tipus << " " << h._dia << ": " << h._hora << "h" << endl;
+  os << h.ass << ": " << std::endl <<
+  h._grup << h._tipus << " " << h._dia << ": " << h._hora << "h" << std::endl;
   return os;
 }
-
 
 
 class Horari{
 public:
   Horari(){
     _valor = 0;
-    h = vector<vector<HoraClasse>>(5,vector<HoraClasse>(13));
+    h = std::vector<std::vector<HoraClasse>>(5,std::vector<HoraClasse>(13));
     for(int i = 0; i < h.size(); ++i){
-      for(int j = 0; j < h[i].size(); ++j){
-        h[i][j] = HoraClasse(j+8,false,"",-1,teoria,dies[i]);
-      }
+      for(int j = 0; j < h[i].size(); ++j)
+        h[i][j] = HoraClasse(j+8,-1,teoria,dies[i],false,"");
     }
   }
   bool encaixa(const HoraClasse& hora){
     return not h[hora.dia()][hora.hora()-8].ocupada();
   }
-  bool encaixaAssig(const vector<HoraClasse>& assig){
+  bool encaixaAssig(const std::vector<HoraClasse>& assig){
     int index = findInVector(assigs,assig[0].assignatura());
     if(index != -1){
       int index2 = findInVector(grups,assig[0].grup());
@@ -101,37 +95,31 @@ public:
     }
     return true;
   }
-  void afegeix(const vector<HoraClasse>& hores){
+  void afegeix(const std::vector<HoraClasse>& hores){
     int index = findInVector(assigs,hores[0].assignatura());
     if(index == -1){
       assigs.push_back(hores[0].assignatura());
       grups.push_back(hores[0].grup());
     }
-    //cout << "afegeix" << endl;
     for(int i = 0; i < hores.size(); ++i){
       h[hores[i].dia()][hores[i].hora()-8] = hores[i];
       h[hores[i].dia()][hores[i].hora()-8].ocupa();
     }
-    
   }
   int nassig(){return assigs.size();}
   int valor()const { return _valor; }
-  bool operator<(const Horari& other)const{
-    return _valor > other._valor;
-  }
-
+  bool operator<(const Horari& other)const{ return _valor > other._valor; }
   void print()const{
-    //cout << "print horari" << endl;
-    cout << "\tDilluns" << "\tDimarts" << "\tDimecr" << "\tDijous" << "\tDivend" << endl;
+    std::cout << "\tDilluns" << "\tDimarts" << "\tDimecr" << "\tDijous" << "\tDivend" << std::endl;
 
     for(int j = 0; j < h[0].size(); ++j){
-      cout << 8+j << "h\t";
+      std::cout << 8+j << "h\t";
       for(int i = 0; i < h.size(); ++i){
         if(h[i][j].ocupada()) h[i][j].print();
-        else cout << "  -  ";
-        cout << "\t";
+        else std::cout << "  -  ";
+        std::cout << "\t";
       }
-      cout << endl;
+      std::cout << std::endl;
     }
   }
   void calculaValor(){
@@ -169,15 +157,15 @@ public:
   }
 private:
   template<typename T>
-  int findInVector(const vector<T>& vec, const T& thing){
+  int findInVector(const std::vector<T>& vec, const T& thing){
     for(int i = 0; i < vec.size(); ++i){
       if(vec[i] == thing) return i;
     }
     return -1;
   }
   std::vector<std::vector<HoraClasse>> h;
-  vector<string> assigs;
-  vector<int> grups;
+  std::vector<std::string> assigs;
+  std::vector<int> grups;
   int _valor;
 };
 
