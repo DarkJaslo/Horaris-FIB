b4e0b398d2c4760dd1af0d7b290b86ee4e74fb22
Jon <jon.campillo@gmail.com>
2023-09-14 17:58:16 +0200

    separated from HorarisParser

---
+++ src/Parser.cc
@@ -0,0 +1,181 @@
+#include "Parser.hh"
+using namespace jaslo;
+
+//Parser
+
+Parser::Parser()
+{
+  _curIndex   = 0;
+  _count      = 0;
+  _openedFile = false;
+}
+
+int Parser::count()const{  return _count; }
+
+void Parser::openFile(const std::string& filename)
+{
+  _file.open(filename.c_str());
+  _openedFile = true;
+}
+
+void Parser::getCount()
+{
+  if(not _openedFile){
+    std::cerr << "Trying to get the number of groups without opening a file" << std::endl;
+    exit(1);
+  }
+  while(not _file.eof())
+  {
+    std::string line;
+    getline(_file,line);
+    std::istringstream iss(line);
+    std::string aux;
+    iss >> aux;
+    
+    if(aux.length() < 7) continue;
+    if(aux[0] == '"')
+    {
+      if(aux.substr(1,5) == "count")
+      {
+        iss >> _count;
+        getline(_file,line); //Leaves everything ready
+        return;
+      }
+    }
+  }
+}
+
+bool Parser::readHorariObj(HorariObj& o)
+{
+  o = HorariObj();
+  std::string        line;
+  std::istringstream iss;
+  std::string        aux;
+  std::string        constant;
+  getline(_file,line); //  Reads {
+
+  getline(_file,line); //  Reads "codi_assig"
+  iss = std::istringstream(line);
+  iss >> aux;
+  constant = "codi_assig";
+  if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+  {
+    iss >> aux;
+    o._code = aux.substr(1,aux.length()-3);
+  }
+  else{
+    printParseError("wrong codi_assig keyword");
+  }
+
+  getline(_file,line); //  Reads "grup"
+  iss = std::istringstream(line);
+  iss >> aux;
+  constant = "grup";
+  if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+  {
+    iss >> aux;
+    if(aux.substr(1,aux.length()-3).length() <= 2) 
+      o._group = stoi(aux.substr(1,aux.length()-3));
+    else o._group = 0;
+  }
+  else{
+    printParseError("wrong grup keyword");
+  }
+
+  getline(_file,line); //  Reads "dia_setmana"
+  iss = std::istringstream(line);
+  iss >> aux;
+  constant = "dia_setmana";
+  if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+  {
+    int auxint;
+    iss >> auxint;
+    const Weekday totsDies[5] = {Weekday::monday,Weekday::tuesday,Weekday::wednesday,Weekday::thursday,Weekday::friday};
+    o._day = totsDies[auxint-1];
+  }
+  else{
+    printParseError("wrong dia_setmana keyword");
+  }
+
+  getline(_file,line); //  Reads "inici"
+  iss = std::istringstream(line);
+  iss >> aux;
+  constant = "inici";
+  if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+  {
+    iss >> aux;
+    std::string hour = aux.substr(1,aux.length()-3);
+    int h = 0;
+    int index = 0;
+    while(hour[index] != ':')
+    {
+      h*=10;
+      h += hour[index]-'0';
+      index++;
+    }
+    o._hours.push_back(h);
+
+    getline(_file,line); //  Reads "durada"
+    iss = std::istringstream(line);
+    iss >> aux;
+    constant = "durada";
+    if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+    {
+      int durada;
+      iss >> durada;
+      durada--; //The first hour is already included
+      int counter = 1;
+      while(counter <= durada)
+      {
+        o._hours.push_back(h+counter);
+        counter++;
+      }
+    }
+    else{
+      printParseError("wrong durada keyword");
+    }
+  }
+  else{
+    printParseError("wrong inici keyword");
+  }
+
+  
+  getline(_file,line); //  Reads "tipus"
+  iss = std::istringstream(line);
+  iss >> aux;
+  constant = "tipus";
+  if(isCorrect(constant.length()+3,constant.length(),aux,constant))
+  {
+    iss >> aux;
+    aux = aux.substr(1,aux.length()-3);
+    if(aux == "T") o._type = ClassType::theory;
+    else if(aux == "L") o._type = ClassType::lab;
+    else o._type = ClassType::problems;
+  }
+  else{
+    printParseError("wrong tipus keyword");
+  }
+
+
+  getline(_file,line); //  Reads "aules"
+  getline(_file,line); //  Reads "idioma"
+  getline(_file,line); //  Reads "},"
+  iss = std::istringstream(line);
+  iss >> aux;
+  return aux == "},";
+}
+
+
+//Private parser
+
+bool Parser::isCorrect(int expectedSize, int substrSize, const std::string& str, const std::string& substr)const
+{
+  if(str.length() < expectedSize) return false;
+  return str.substr(1,substrSize) == substr;
+}
+
+void Parser::printParseError(const std::string& extraInfo = "")
+{
+  std::cerr << "The parser has found a problem / unexpected input in FIB_DATA.txt. Extra info: " << extraInfo << std::endl;
+  exit(1);
+}
---
+++ src/Parser.hh
@@ -0,0 +1,61 @@
+#ifndef HorarisParser_hh
+#define HorarisParser_hh
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "Horari.hh"
+
+/*
+  My attempt at writing a static parser for a FIB schedule generator.
+  It can read, parse and store the Class list the FIB API provides (https://api.fib.upc.edu).
+  
+  That is the only thing it can do, so don't try to use it for anything else. It only "looks generic" because that way it's more reusable.
+*/
+
+namespace jaslo{
+
+class Parser{
+public:
+
+  Parser();
+
+  /*
+    The number of different HorariObj that will have to be read
+  */
+  int count()const;
+
+  /*
+    Opens a local file with name [filename]
+  */
+  void openFile(const std::string& filename);
+
+  /*
+    Requires having opened a file.
+    Reads everything until the "count": line in the file, and stores its value.
+  */
+  void getCount();
+
+  /*
+    Requires having opened a file and having called getCount() once.
+    Reads an HorariObj. Returns false if it read the last one, and true otherwise
+  */
+  bool readHorariObj(HorariObj& o);
+
+private:
+
+  bool isCorrect(int expectedSize, int substrSize, const std::string& str, const std::string& substr)const;
+  void printParseError(const std::string& extraInfo);
+
+
+  std::ifstream _file;
+  std::string   _info;
+  int           _curIndex;
+  int           _count;
+  bool          _openedFile;
+};
+
+}; //Namespace end
+#endif
