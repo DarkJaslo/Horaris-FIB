1dd0de24f43eaa97194fe4a4a1bccf9d1a592d98
Jon <jon.campillo@gmail.com>
2023-09-14 19:30:52 +0200

    httplib wrap

---
+++ src/HTTPSGetter.cc
@@ -0,0 +1,50 @@
+#include "HTTPSGetter.hh"
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "../lib/httplib.h"
+
+#include <fstream>
+#include <iostream>
+using namespace jaslo;
+
+bool HTTPSGetter::get(const std::string& serverUrl, const std::string& path, const std::string& outputFilename)
+{
+	httplib::Client cli(serverUrl);
+
+	if(auto res = cli.Get(path)){
+		if(res->status == 200)
+		{
+			std::fstream file;
+			file.open(outputFilename,std::ios::out | std::ios::trunc);
+			if(file.is_open())
+			{
+				replaceQuotations(res->body);
+				file << res->body;
+				file.close();
+				return true;
+			}
+			else{
+				std::cerr << "File " << outputFilename << " is not open" << std::endl;
+			}
+		}
+		else{
+			std::cerr << "GET request failed with code " << res->status << std::endl;
+		}
+	}
+	else{
+		auto err = res.error();
+		std::cerr << "HTTP error: " << httplib::to_string(err) << std::endl;
+	}
+
+	return false;
+}
+
+void HTTPSGetter::replaceQuotations(std::string& str)
+{
+	size_t found = str.find("&quot;");
+
+	while(found != std::string::npos)
+	{
+		str.replace(found,6,"\"");
+		found = str.find("&quot;",found+1);
+	}
+}
---
+++ src/HTTPSGetter.hh
@@ -0,0 +1,24 @@
+#ifndef HTTPSGetter_hh
+#define HTTPSGetter_hh
+
+#include <string>
+
+namespace jaslo{
+
+class HTTPSGetter{
+
+public:
+
+	//Gets path from serverUrl and outputs it to outputFilename
+	static bool get(const std::string& serverUrl, const std::string& path, const std::string& outputFilename);
+
+private:
+
+	//Replaces every instance of '&quot;' in str with '"'
+	static void replaceQuotations(std::string& str);
+
+};
+
+
+}; //Namespace end
+#endif
