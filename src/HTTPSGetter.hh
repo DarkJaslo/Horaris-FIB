c3dc205d68ecc9967d13dfab3d50ef44e3581874
Jon <jon.campillo@gmail.com>
2023-09-14 19:46:34 +0200

    switched to spaces

--- src/HTTPSGetter.hh
+++ src/HTTPSGetter.hh
@@ -9,13 +9,13 @@ class HTTPSGetter{
 
 public:
 
-	//Gets path from serverUrl and outputs it to outputFilename
-	static bool get(const std::string& serverUrl, const std::string& path, const std::string& outputFilename);
+  //Gets path from serverUrl and outputs it to outputFilename
+  static bool get(const std::string& serverUrl, const std::string& path, const std::string& outputFilename);
 
 private:
 
-	//Replaces every instance of '&quot;' in str with '"'
-	static void replaceQuotations(std::string& str);
+  //Replaces every instance of '&quot;' in str with '"'
+  static void replaceQuotations(std::string& str);
 
 };
 
