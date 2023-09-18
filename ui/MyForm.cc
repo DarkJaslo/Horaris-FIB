6f4f077337e01f06e19cbef716ff8ecfc59c1227
Jon <jon.campillo@gmail.com>
2023-09-18 19:43:11 +0200

    add comments to show progress

--- ui/MyForm.cc
+++ ui/MyForm.cc
@@ -3,6 +3,10 @@
 MyForm::MyForm(QWidget* parent) : QWidget(parent)
 {
   ui.setupUi(this);
+  
+  std::cout << "setup ui" << std::endl;
   ui.appWidget->getSemesters();
+  std::cout << "got semesters" << std::endl;
   ui.appWidget->getData();
+  std::cout << "got data" << std::endl;
 }
