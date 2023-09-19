54bc86a972ad5a606a2554005fd80bb1332311db
Jon <jon.campillo@gmail.com>
2023-09-20 00:51:52 +0200

    passes listWidget to appWidget

--- ui/MyForm.cc
+++ ui/MyForm.cc
@@ -3,10 +3,15 @@
 MyForm::MyForm(QWidget* parent) : QWidget(parent)
 {
   ui.setupUi(this);
+
+  ui.appWidget->receiveListWidget(ui.listWidget);
+  //ui.listWidget->setSortingEnabled(true);
   
   std::cout << "setup ui" << std::endl;
   ui.appWidget->getSemesters();
   std::cout << "got semesters" << std::endl;
   ui.appWidget->getData();
   std::cout << "got data" << std::endl;
+
+  
 }
