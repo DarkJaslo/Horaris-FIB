737a2eb54faa467534da064db99deb36bc2c5aaf
Jon <jon.campillo@gmail.com>
2023-09-21 19:33:42 +0200

    receiveSelectionLists() and focusPolicy

--- ui/MyForm.cc
+++ ui/MyForm.cc
@@ -5,8 +5,12 @@ MyForm::MyForm(QWidget* parent) : QWidget(parent)
   ui.setupUi(this);
 
   ui.appWidget->receiveListWidget(ui.listWidget);
+  ui.appWidget->receiveSelectionLists(ui.listInclude, ui.listAlways);
   ui.appWidget->receiveStudiesBox(ui.studiesComboBox);
   //ui.listWidget->setSortingEnabled(true);
+  ui.listWidget->setFocusPolicy(Qt::FocusPolicy::ClickFocus);
+  ui.listInclude->setFocusPolicy(Qt::FocusPolicy::ClickFocus);
+  ui.listAlways->setFocusPolicy(Qt::FocusPolicy::ClickFocus);
   
   std::cout << "setup ui" << std::endl;
   ui.appWidget->getSemesters();
